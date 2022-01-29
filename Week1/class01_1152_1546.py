6d6bcd7c15c1916b1105a3c0684f5cab2c1b0265
jjs0211 <>
2022-01-29 15:20:51 +0000

    test

--- Week1/class01_1152_1546.py
+++ Week1/class01_1152_1546.py
@@ -6,5 +6,14 @@
 #   arr.append(int(input()))
 # print(max(arr), arr.index(max(arr))+1) // 2562 최댓값
 
+# a = int(input())
+# b = int(input())
+# c = int(input())
+
+# mul = a*b*c
+
+# for i in range(10):
+#   print(str(mul).count(str(i))) // 2577 숫자의 개수
+
 
 
