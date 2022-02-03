0bef4408c668cbe298e8fe166f6020692d3da536
jjs0211 <jjs_0211_@naver.com>
2022-02-03 14:31:27 +0000

    단어정렬

---
+++ Baekjoon/Class02/1181_단어정렬.py
@@ -0,0 +1,10 @@
+n = int(input())
+words_list = []
+for _ in range(n):
+  words = input()
+  words_list.append(words)
+words_list = list(set(words_list))
+result = sorted(words_list, key = lambda word: (len(word), word))
+
+for i in result:
+  print(i)
