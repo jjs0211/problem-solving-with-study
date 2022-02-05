10ca5f65b79ee1d4aaff54cdc12c992f1e1e0758
jjs0211 <jjs_0211_@naver.com>
2022-02-05 14:50:22 +0000

    통계학

--- Baekjoon/Class02/2108_통계학.py
+++ Baekjoon/Class02/2108_통계학.py
@@ -1,31 +1,33 @@
-n = int(input())
+from collections import Counter
+import sys
+
+n = int(sys.stdin.readline())
 num_list = []
 for _ in range(n):
-  num = int(input())
+  num = int(sys.stdin.readline())
   num_list.append(num)
 
 sorted_num_list = sorted(num_list)
 
-print(round(sum(num_list)/len(num_list))) # 평균
+print(round(sum(num_list)/n)) # 평균
 print(sorted_num_list[len(num_list)//2]) # 중앙값
 
-# num_dict = {}
-# for i in sorted_num_list:
-#   if i not in num_dict:
-#     num_dict[i] = 1
-#   else:
-#     num_dict[i] += 1
-
-# print(num_dict)
-num_count_list = []
-for i in sorted_num_list:
-  num_count_list.append(sorted_num_list.count(i))
-
-if num_count_list.count(max(num_count_list)) >= 2:
-  print(sorted_num_list[num_count_list.index(max(num_count_list))])
-  
+c_num_list = Counter(sorted_num_list).most_common()
+if len(c_num_list) >1:
+  if c_num_list[0][1] == c_num_list[1][1]:
+    print(c_num_list[1][0])
+  else:
+    print(c_num_list[0][0])
 else:
-  print(sorted_num_list[1])
+  print(c_num_list[0][0])
 
 
-print(max(num_list) - min(num_list)) # 범위
+# print(sorted_num_list[-1] - sorted_num_list[0]) # 범위
+
+
+# Counter().most_common(숫자)
+# 빈도수가 높은 값을 숫자만큼 가져옴(튜플 형태로)
+
+# from collections import Counter
+# ls = [1,1,2,3,4,5,5,6,7,8]
+# c_ls = Counter(ls).most_common(2) // [(1,2), (5,2)]
