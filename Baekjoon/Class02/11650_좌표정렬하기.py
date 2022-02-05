c2a67cdb7e40b9522fa397d9496d59373c34878d
jjs0211 <jjs_0211_@naver.com>
2022-02-05 02:27:29 +0000

    좌표정렬하기

---
+++ Baekjoon/Class02/11650_좌표정렬하기.py
@@ -0,0 +1,9 @@
+n = int(input())
+xy_list = []
+for _ in range(n):
+  x, y = map(int, input().split())
+  xy_list.append((x,y))
+
+sorted_list = sorted(xy_list, key = lambda x: (x[0], x[1]))
+for i in sorted_list:
+  print(i[0], i[1])
