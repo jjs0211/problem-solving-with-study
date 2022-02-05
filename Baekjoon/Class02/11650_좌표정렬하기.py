1a536d05f1cdd2f48d19c5af650ea551097eb650
jjs0211 <jjs_0211_@naver.com>
2022-02-05 02:33:24 +0000

    좌표정렬하기

--- Baekjoon/Class02/11650_좌표정렬하기.py
+++ Baekjoon/Class02/11650_좌표정렬하기.py
@@ -6,4 +6,16 @@ for _ in range(n):
 
 sorted_list = sorted(xy_list, key = lambda x: (x[0], x[1]))
 for i in sorted_list:
-  print(i[0], i[1])
+  print(i[0], i[1]) # 4576ms
+
+
+import sys
+n = int(sys.stdin.readline())
+xy_list = []
+for _ in range(n):
+  x, y = map(int, sys.stdin.readline().split())
+  xy_list.append((x,y))
+
+sorted_list = sorted(xy_list, key = lambda x: (x[0], x[1]))
+for i in sorted_list:
+  print(i[0], i[1]) # 396ms
