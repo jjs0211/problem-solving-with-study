ca069c9b9a68f99440fd695a12479b8dbec98add
jjs0211 <jjs_0211_@naver.com>
2022-02-16 12:19:49 +0000

    4주차

---
+++ Programmers/Level1/신고 결과 받기.py
@@ -0,0 +1,33 @@
+def solution(id_list, report, k):
+    report = set(report) # 한 사람이 같은 사람에게 신고 여러번 한 경우는 한 번만 쳐주므로 set으로 중복 제거
+    report = list(report)
+    reporter = []
+    reported = []
+    idx_reported=[]
+    result = {}
+    for i in range(len(report)):
+        reporter.append(report[i].split()[0]) # 신고자 리스트
+        reported.append(report[i].split()[1]) # 피신고자 리스트
+    set_reported = set(reported)
+    set_reported = list(set_reported) # 피신고자 count 편하게 세기 위해 중복을 지운 리스트를 하나 만듦
+    for j in range(len(set_reported)):
+        if reported.count(set_reported[j]) >= k: # 신고를 k번 이상 당했다면
+            idx_reported.extend(list(filter(lambda x: reported[x] == set_reported[j], range(len(reported))))) # 피신고자 리스트에서 중복되는 값들의 index를 모두 받아옴(신고자 리스트와 피신고자 리스트의 index가 같으니까)
+            # x = [0, 1, 2, 3, 4] 에서 reported[x] == set_reported[j] 즉, k번 이상 신고당한 사람의 인덱스를 뽑아옴
+    for k in idx_reported:
+        if reporter[k] not in result:
+            result[reporter[k]] = 1
+        else:
+            result[reporter[k]]+=1
+    print(reporter)
+    print(reported)
+    print(result) # result에는 신고자가 몇 번 신고했는지 나옴
+    real_result = []
+    for x in id_list:
+        if x in result:
+            real_result.append(result[x])
+        else:
+            real_result.append(0)
+    print(real_result)
+
+solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)
