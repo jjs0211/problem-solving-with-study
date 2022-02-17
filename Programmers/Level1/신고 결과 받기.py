8d491c3e4142f808af2832a705c2421e7c4de5c2
jjs0211 <jjs_0211_@naver.com>
2022-02-17 14:17:24 +0000

    4주차

--- Programmers/Level1/신고 결과 받기.py
+++ Programmers/Level1/신고 결과 받기.py
@@ -31,3 +31,29 @@ def solution(id_list, report, k):
     print(real_result)
 
 solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)
+
+
+
+# def solution(id_list, report, k):
+#     users = {name:[] for i, name in enumerate(id_list)}
+#     ban_users = {name:0  for i, name in enumerate(id_list)}
+#     # print(users)
+#     # print(ban_users)
+    
+#     for temp in set(report):
+#         user, ban_user = temp.split(" ")
+#         users[user].append(ban_user)
+#         ban_users[ban_user] += 1
+    
+#     # print(users)
+#     # print(ban_users)
+    
+#     answer = [0 for _ in range(len(id_list)) ]
+#     print(answer)
+#     for key, values in users.items():
+#         for value in values:
+#             if ban_users[value] >= k:
+#                 answer[id_list.index(key)] += 1
+#     # print(answer)
+    
+#     return answer
