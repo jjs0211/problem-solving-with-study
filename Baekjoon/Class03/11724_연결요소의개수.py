6e28cca08ff51af162dca45f4118220ed968870e
jjs0211 <jjs_0211_@naver.com>
2022-03-10 07:19:28 +0000

    복습

--- Baekjoon/Class03/11724_연결요소의개수.py
+++ Baekjoon/Class03/11724_연결요소의개수.py
@@ -26,3 +26,33 @@ for i in range(1, n+1):
         bfs(i)
         cnt+=1
 print(cnt)
+
+# from collections import deque
+# n, m = map(int, input().split())
+
+# graph = [[] for _ in range(n+1)]
+# visited = [False] * (n+1)
+
+# for _ in range(m):
+#     a, b = map(int, input().split())
+#     graph[a].append(b)
+#     graph[b].append(a)
+
+# def bfs(start):
+#     visited[start] = True
+#     queue = deque()
+#     queue.append(start)
+
+#     while queue:
+#         a = queue.popleft()
+#         for i in graph[a]:
+#             if not visited[i]:
+#                 visited[i] = True
+#                 queue.append(i)
+
+# cnt = 0
+# for i in range(1, n+1):
+#     if not visited[i]:
+#         bfs(i)
+#         cnt += 1
+# print(cnt)
