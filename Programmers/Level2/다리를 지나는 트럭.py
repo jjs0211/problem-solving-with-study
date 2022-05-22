e39007f472b91baa49fd6d672017c8c67525073f
jjs0211 <jjs_0211_@naver.com>
2022-05-22 13:52:04 +0000

    0522

---
+++ Programmers/Level2/다리를 지나는 트럭.py
@@ -0,0 +1,15 @@
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    bridge = [0] * bridge_length
+    time = 0
+    
+    while bridge:
+        bridge.pop(0)
+        time += 1
+        if truck_weights:
+            if sum(bridge) + truck_weights[0] <= weight:
+                bridge.append(truck_weights.pop(0))
+            else:
+                bridge.append(0)
+
+    return time
